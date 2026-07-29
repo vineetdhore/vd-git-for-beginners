45c09e2fc9b136bb732e56e3a08eaf254fdccbcb
Vineet Dhore <vineetdhore@yahoo.co.in>
2026-07-29 10:38:55 +0530

    added add_nums function and main script entry point

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils.calculator import add_nums
+
+if __name__ == "__main__":
+    result = add_nums(3, 5)
+    print(result)
---
+++ utils/calculator.py
@@ -0,0 +1,3 @@
+def add_nums(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
