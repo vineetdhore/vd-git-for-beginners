2f4bb071cfd1ba8fc7b1bed529564b43f82ece3d
Vineet Dhore <vineetdhore@yahoo.co.in>
2026-07-31 18:06:03 +0530

    added and formatted print statments to main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums,subtract_nums,multiply_nums,divide_nums
 
 if __name__ == "__main__":
-    result = add_nums(3, 5)
-    print(result)
+    print("Addition:", add_nums(3, 5))
+    print("Subtraction:", subtract_nums(5, 3))
+    print("Multiplication:", multiply_nums(3, 5))
+    print(f"Division:{divide_nums(5, 3):.2f}")
+    
